e1108dccda88346e4be2f3685b30231b4022736a
Marat <user2589@users.noreply.github.com>
2018-10-30 22:33:20 -0400

    fixed dependencies

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     # whenever you're updating the next three lines
     # please also update oscar.py
     name="strudel.utils",
-    version="0.2.2",  # make sure to update __init__.py.__version__
+    version="0.2.3",  # make sure to update __init__.py.__version__
     author='Marat (@cmu.edu)',
     description="Various utils used internally by strudel.* packages",
     long_description=open('README.md').read(),
--- stutils/__init__.py
+++ stutils/__init__.py
@@ -7,7 +7,7 @@ except ImportError:
     settings = object()
 
 # make sure to update setup.py
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 CONFIG = {}
 
 
